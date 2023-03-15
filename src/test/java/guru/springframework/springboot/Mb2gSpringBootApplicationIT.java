21a3ca4f2859eb194589658a43fcc293a504d0dc
kerleau <37364328+kerleau@users.noreply.github.com>
2023-03-15 18:13:47 +0000

    add Integration Test class and plugin to start spring boot to do the test and stop

---
+++ src/test/java/guru/springframework/springboot/Mb2gSpringBootApplicationIT.java
@@ -0,0 +1,18 @@
+package guru.springframework.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mb2gSpringBootApplicationIT {
+
+	@Test
+	public void contextLoads() {
+        System.out.println("My IT Ran.................7777777777777");
+	}
+
+}
+
